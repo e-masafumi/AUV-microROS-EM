77f04ced6ee4fafbb8566135aae122d343065ac4
Masafumi Edamoto <masafumi.edamoto@gmail.com>
2024-01-24 19:05:10 +0900

    log in bin.

--- main.cpp
+++ main.cpp
@@ -19,6 +19,8 @@
 const uint LED_PIN = 25;
 const uint I2C1_SDA_PIN = 14;
 const uint I2C1_SCL_PIN = 15;
+volatile bool exeFlag = false;
+struct repeating_timer st_timer;
 pico_pwm pwm;
 pico_i2c i2c;
 MS5837_02BA MS5837;
@@ -27,29 +29,54 @@ BNO055 BNO055;
 bool reserved_addr(uint8_t addr){
     return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
 }
+int64_t alarm_callback(alarm_id_t id, void *user_data) {
+	printf("Timer %d fired!\n", (int) id);
+	exeFlag = true;
+	// Can return a value here in us to fire in the future
+	return 0;
+}
+
+bool repeating_timer_callback(struct repeating_timer *t) {
+//	printf("Repeat at %lld\n", time_us_64());
+	exeFlag = true;
+	return true;
+}
 
 int main(){
 //	bi_decl(bi_program_description("This is a test binary."));
 //	bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
 
 	uint8_t data=0;
-	uint32_t timeBuff_32=0;
+//	uint32_t timeBuff_32=0;
 	uint64_t timeBuff_64=0;
 
-	double outTemp=0.0;
+/*	double outTemp=0.0;
 	double outPress=0.0;
 	double xAccel=0.0;
 	double yAccel=0.0;
 	double zAccel=0.0;
 	double xMag=0.0;
 	double yMag=0.0;
-	double zMag=0.0;
+	double zMag=0.0;*/
 	double pSurface=0.0;
 	double tempSurface=0.0;
 	double turgetDepth=0.0;
 	int i=0;
 
-	//SD
+	struct sensorsData{
+		uint32_t timeBuff_32=0;
+		double outTemp=0.0;
+		double outPress=0.0;
+		double xAccel=0.0;
+		double yAccel=0.0;
+		double zAccel=0.0;
+		double xMag=0.0;
+		double yMag=0.0;
+		double zMag=0.0;
+	};
+	
+	struct sensorsData logData;
+
 	FRESULT fr;
 	FATFS fs;
 	FIL fil;
@@ -130,33 +157,33 @@ int main(){
 
 
 
-    // Initialize SD card
-    if (!sd_init_driver()) {
-        printf("ERROR: Could not initialize SD card\r\n");
-        while (true);
-    }
+  // Initialize SD card
+  if (!sd_init_driver()) {
+		printf("ERROR: Could not initialize SD card\r\n");
+		while (true);
+	}
 
-    // Mount drive
-    fr = f_mount(&fs, "0:", 1);
-    if (fr != FR_OK) {
-        printf("ERROR: Could not mount filesystem (%d)\r\n", fr);
-        while (true);
-    }
+  // Mount drive
+  fr = f_mount(&fs, "0:", 1);
+  if (fr != FR_OK) {
+		printf("ERROR: Could not mount filesystem (%d)\r\n", fr);
+		while (true);
+  }
 
-    // Open file for writing ()
-    fr = f_open(&fil, filename, FA_WRITE | FA_CREATE_ALWAYS);
-    if (fr != FR_OK) {
-        printf("ERROR: Could not open file (%d)\r\n", fr);
-        while (true);
-    }
+  // Open file for writing ()
+	fr = f_open(&fil, filename, FA_WRITE | FA_CREATE_ALWAYS);
+	if (fr != FR_OK) {
+		printf("ERROR: Could not open file (%d)\r\n", fr);
+		while (true);
+  }
 
 	// Write something to file
-    ret = f_printf(&fil, "Time, Temp, Press, ax, ay, az, Bx, By, Bz\r\n");
-    if (ret < 0) {
-        printf("ERROR: Could not write to file (%d)\r\n", ret);
-        f_close(&fil);
-        while (true);
-    }
+  ret = f_printf(&fil, "Test\r\n");
+	if (ret < 0) {
+		printf("ERROR: Could not write to file (%d)\r\n", ret);
+		f_close(&fil);
+		while (true);
+	}
 /*    ret = f_printf(&fil, "of writing to an SD card.\r\n");
     if (ret < 0) {
         printf("ERROR: Could not write to file (%d)\r\n", ret);
@@ -165,59 +192,75 @@ int main(){
     }
 */
     // Close file
-    fr = f_close(&fil);
-    if (fr != FR_OK) {
-        printf("ERROR: Could not close file (%d)\r\n", fr);
-        while (true);
-    }
+  fr = f_close(&fil);
+  if (fr != FR_OK) {
+		printf("ERROR: Could not close file (%d)\r\n", fr);
+    while (true);
+  }
 
-    // Open file for reading
-    fr = f_open(&fil, filename, FA_READ);
-    if (fr != FR_OK) {
-        printf("ERROR: Could not open file (%d)\r\n", fr);
-        while (true);
-    }
+  // Open file for reading
+  fr = f_open(&fil, filename, FA_READ);
+  if (fr != FR_OK) {
+		printf("ERROR: Could not open file (%d)\r\n", fr);
+    while (true);
+  }
 
     // Print every line in file over serial
-    printf("Reading from file '%s':\r\n", filename);
-    printf("---\r\n");
-    while (f_gets(buf, sizeof(buf), &fil)) {
-        printf(buf);
-    }
-    printf("\r\n---\r\n");
+	printf("Reading from file '%s':\r\n", filename);
+  printf("---\r\n");
+  while (f_gets(buf, sizeof(buf), &fil)) {
+		printf(buf);
+  }
+  printf("\r\n---\r\n");
 
-    // Close file
-    fr = f_close(&fil);
-    if (fr != FR_OK) {
-        printf("ERROR: Could not close file (%d)\r\n", fr);
-        while (true);
-    }
+  // Close file
+  fr = f_close(&fil);
+  if (fr != FR_OK) {
+		printf("ERROR: Could not close file (%d)\r\n", fr);
+		while (true);
+  }
 
     // Unmount drive
 //    f_unmount("0:");
 
 
     // Open file for writing ()
-    fr = f_open(&fil, filename, FA_WRITE | FA_OPEN_ALWAYS);
-    if (fr != FR_OK) {
-        printf("ERROR: Could not open file (%d)\r\n", fr);
-        while (true);
-    }
-    ret = f_printf(&fil, "Time, Temp, Press, ax, ay, az, Bx, By, Bz\r\n");
+	fr = f_open(&fil, filename, FA_WRITE | FA_CREATE_ALWAYS);
+	if (fr != FR_OK) {
+		printf("ERROR: Could not open file (%d)\r\n", fr);
+    while (true);
+  }
+//    ret = f_printf(&fil, "Time, Temp, Press, ax, ay, az, Bx, By, Bz\r\n");
+//    ret = f_write(&fil, "Time, Temp, Press, ax, ay, az, Bx, By, Bz\r\n");
 
+
+	add_repeating_timer_us(100*1000, repeating_timer_callback, NULL, &st_timer);
 
 
 	while(1) {
-		MS5837.readTempPress(i2c1, &outTemp, &outPress);
-		sleep_ms(100);
-		BNO055.readAccel(i2c1, &xAccel, &yAccel, &zAccel);
-		BNO055.readMag(i2c1, &xMag, &yMag, &zMag);
-		timeBuff_32 = time_us_32();
-		printf("%d, %f, %f\n",timeBuff_32, outTemp, outPress);
+		if(exeFlag == false){
+//			printf("Skipped\n");
+			continue;
+		}
+		
+		logData.timeBuff_32 = time_us_32();
+		MS5837.readTempPress(i2c1, &logData.outTemp, &logData.outPress);
+		BNO055.readAccel(i2c1, &logData.xAccel, &logData.yAccel, &logData.zAccel);
+		BNO055.readMag(i2c1, &logData.xMag, &logData.yMag, &logData.zMag);
+		printf("%d, %f, %f\n",logData.timeBuff_32, logData.outTemp, logData.outPress);
 
 		// Write something to file
 //    ret = f_printf(&fil, "%d, %lf, %lf,%lf,%lf,%lf,%lf,%lf,%lf\r\n", timeBuff_32, outTemp, outPress,xAccel,yAccel,zAccel,xMag,yMag,zMag);
-    ret = f_writef(&fil, "%d, %lf, %lf,%lf,%lf,%lf,%lf,%lf,%lf\r\n", timeBuff_32, outTemp, outPress,xAccel,yAccel,zAccel,xMag,yMag,zMag);
+//		writeBuff[] = {timeBuff_32, outTemp, outPress, xAccel, yAccel, zAccel, xMag, yMag, zMag};
+//		writeSize=sizeof(writeBuff);
+		size_t writeSize=sizeof(logData);
+		unsigned int writtenSize=0;
+    ret = f_write(&fil, &logData, int(writeSize), &writtenSize);
+		if(writeSize != writtenSize){
+			printf("DISK FULL");
+		}
+		else{
+		}	
 /*    ret = f_printf(&fil, "%lf, %lf, %lf,", xAccel, yAccel, zAccel);
     ret = f_printf(&fil, "%lf, %lf, %lf", xMag, yMag, zMag);
     ret = f_printf(&fil, "\r\n");*/
@@ -277,5 +320,8 @@ int main(){
 		else{
 			i++;
 		}
+		logData.timeBuff_32 = time_us_32();
+		printf("%d, %f, %f\n\n",logData.timeBuff_32, logData.outTemp, logData.outPress);
+		exeFlag = false;
 	}
 }
